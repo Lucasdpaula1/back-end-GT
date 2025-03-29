19c1c1a8eb37adea6e79a97264370c48cd9d6971
lucas <lucassidpaula@gmail.com>
2025-03-28 22:02:07 -0300

    feat: consumindo a api do ricky mory

--- api/server.express.js
+++ api/server.express.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
-app.get("/getAll", (req, res) => {
+app.get("/getAll/:quantity", async (req, res) => {
   try {
-    const dataDb = [{ gt: ["lucas", "alex", "carlos", "ray", "samuel"] }];
-    res.status(200).json(dataDb);
+    const quantity = req.params.quantity;
+    const quantityArr = [];
+    for (let i = 0; i < quantity; i++) {
+      quantityArr.push(Math.floor(Math.random() * 10));
+    }
+    const response = await fetch(
+      `https://rickandmortyapi.com/api/character/${quantityArr.join(",")}`
+    );
+    const data = await response.json();
+    res.status(200).json(data);
   } catch (e) {
     console.error(e);
   }
